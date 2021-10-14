7978151055eda9948c323402bd6de40f273b20d8
lamperouge <philbohn1791@gmail.com>
2021-10-14 19:04:05 -0400

    product-routes GET by ID functions

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,10 +15,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// GET route to return a Product filtered by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag}],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
